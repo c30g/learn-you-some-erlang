9293c75872fa7601cf942e4390caf90b7f12a289
YuGu Zhang <c30g@qq.com>
2020-10-17 22:16:37 +0800

    update test

--- test/yg_test.erl
+++ test/yg_test.erl
@@ -1,15 +1,22 @@
 %%% system attributes
 -module(yg_test).
--export([add/2, log/1, ellie/0, ret/2]).
+-export([add/2, log/1, ellie/0, ret/2, sublist/2]).
 -compile([debug_info]).
 
 %% custom atrributes.
 -author("Zhang YuGu").
 
-add(X,Y) -> X + Y.
+add(X, Y) -> X + Y.
 
 log(Content) -> io:format(Content).
 
 ellie() -> io:format("hello, Ellie.~n").
 
-ret(X,Y) -> {X,Y}.
+ret(X, Y) -> {X,Y}.
+
+% sublist(L,N) -> reverse(tail_sublist(L,N,[])).
+sublist(L,N) -> tail_sublist(L,N,[]).
+
+tail_sublist(_, 0, SubList) -> SubList;
+tail_sublist([], _, SubList) -> SubList;
+tail_sublist([H|T], N, SubList) when N > 0 -> tail_sublist(T, N-1, [H|SubList]).
