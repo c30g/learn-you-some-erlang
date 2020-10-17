4f2969842e64a83af42da3079ef8a1264c743dd0
YuGu Zhang <c30g@qq.com>
2020-10-17 18:21:19 +0800

    update tests

--- test/yg_test.erl
+++ test/yg_test.erl
@@ -1,6 +1,6 @@
 %%% system attributes
 -module(yg_test).
--export([add/2, log/1, ellie/0]).
+-export([add/2, log/1, ellie/0, ret/2]).
 -compile([debug_info]).
 
 %% custom atrributes.
@@ -11,3 +11,5 @@ add(X,Y) -> X + Y.
 log(Content) -> io:format(Content).
 
 ellie() -> io:format("hello, Ellie.~n").
+
+ret(X,Y) -> {X,Y}.
