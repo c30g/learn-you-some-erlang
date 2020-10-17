66f28bb38925b4c27097cae62614cf0ca39b83d8
YuGu Zhang <c30g@qq.com>
2020-10-17 16:36:35 +0800

    update test functions

--- test/yg_test.erl
+++ test/yg_test.erl
@@ -1,9 +1,13 @@
+%%% system attributes
 -module(yg_test).
+-export([add/2, log/1, ellie/0]).
+-compile([debug_info]).
 
--export([add/2, log/1]).
+%% custom atrributes.
+-author("Zhang YuGu").
 
-add(X,Y) ->
-    X + Y.
+add(X,Y) -> X + Y.
 
-log(Content) ->
-    io:format("first log").
+log(Content) -> io:format(Content).
+
+ellie() -> io:format("hello, Ellie.~n").
